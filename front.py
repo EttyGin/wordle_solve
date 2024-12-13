a2a547a3c6409e7f43d1622c2b165c0ad1ae8c39
EttyGin <ettyg325@gmail.com>
2024-12-13 02:02:48 +0200

    final editing

--- front.py
+++ front.py
@@ -1,11 +1,10 @@
 import dash
-from dash import Input, Output, State, html
+from dash import Input, Output, State, html, dcc
 import dash_bootstrap_components as dbc
 
 from wordle import find_matching_words
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-
 app.layout = html.Div(
     [
         dbc.Container(
@@ -23,6 +22,7 @@ app.layout = html.Div(
                                                     id="pattern",
                                                     type="text",
                                                     placeholder="like: p..n.",
+                                                    style={"width": "100%"},
                                                 ),
                                                 dbc.Label("Enter includes"),
                                                 dbc.Input(
@@ -33,59 +33,69 @@ app.layout = html.Div(
                                                 dbc.Label("Enter excludes"),
                                                 dbc.Input(
                                                     id="exclude",
-                                                    type="התבנית שלך",
+                                                    type="text",
                                                     placeholder="like: xz",
                                                 ),
                                                 dbc.Button(
                                                     "Get Options!",
                                                     id="get",
+                                                    color="primary",
+                                                    className="mt-3",
                                                     style={
-                                                        "margin": "auto",
-                                                        "padding": "20px",
-                                                        "display": "block",  # מוסיף display: block עבור מיקום מדויק יותר
+                                                        "display": "block",
                                                         "width": "100%",
-                                                    },  # מרכז את הכפתור
+                                                    },
+                                                ),
+                                                dbc.Label(
+                                                    id="error_msg",
                                                 ),
                                             ]
-                                        ),
+                                        )
                                     ],
-                                    style={"display": "block"},
-                                ),
+                                    style={
+                                        "border": "1px solid #ccc",
+                                        "border-radius": "5px",
+                                        "padding": "20px",
+                                        "text-align": "center",
+                                        "align-items": "center",
+                                        "flex-direction": "center",
+                                    },
+                                )
                             ],
-                            style={
-                                "flex-direction": "center",
-                                "align-items": "center",
-                                "min-height": "100vh",
-                                
-                                "padding": "50px",
-                                "background-color": "#D9E7FF",
-                            },
-                        ),  # קבע את רוחב העמודה של הטופס ל-6 מתוך 12
+                            width=12,
+                        )
+                    ]
+                ),
+                dbc.Row(
+                    [
                         dbc.Col(
                             [
-                                html.H3(
-                                    "Your Options Are:",
-                                    style={
-                                        "padding": "50px",
-                                        "text-align": "center",
-                                        "background-color": "#99FFDD",
-                                    },
-                                ),
-                                html.Div(
-                                    id="output-container",
-                                    style={
-                                        "flex-direction": "center",
-                                        "align-items": "center",
-                                        "min-height": "100vh",
-                                        "text-align": "center",
-                                        "background-color": "#99FFDD",
-                                    },
+                                html.H3("Your Options Are:", className="text-center"),
+                                dcc.Loading(
+                                    id="loading-1",
+                                    children=[
+                                        html.Div(
+                                            id="output-container",
+                                            style={
+                                                "flex-direction": "center",
+                                                "align-items": "center",
+                                                "min-height": "100vh",
+                                                "text-align": "center",
+                                                "white-space": "pre-wrap",
+                                                "word-wrap": "break-word",
+                                            },
+                                        )
+                                    ],
+                                    type="circle",
                                 ),
                             ],
-                        ),
+                            width=12,
+                        )
                     ]
-                )
-            ]
+                ),
+            ],
+            fluid=True,
+            style={"height": "100vh", "backgroundColor": "#f0f0f0"},
         )
     ]
 )
@@ -93,18 +103,21 @@ app.layout = html.Div(
 
 @app.callback(
     Output("output-container", "children"),
+    Output("error_msg", "children"),
     [Input("get", "n_clicks")],
     [State("pattern", "value"), State("include", "value"), State("exclude", "value")],
     prevent_initial_calls=True,
 )
 def update_output(n_clicks, pattern: str, include: str, exclude: str):
     if n_clicks is None:
-        return ""
-
+        return "", ""
+    if pattern is None:
+        return "", "You have to provide a pattern!"
     print(f"{pattern}\n{include}\n{exclude}")
+
     words: str = find_matching_words(pattern, include, exclude)
 
-    return words
+    return words, ""
 
 
 if __name__ == "__main__":
--- wordle.py
+++ wordle.py
@@ -5,7 +5,12 @@ from nltk.corpus import words
 
 def find_matching_words(pattern: str, include: str, exclude: str) -> str:
     nltk.download("words")
+    if include is None:
+        include = ""
+    if exclude is None:
+        exclude = ""
 
+    print(include, pattern, exclude, "ergfdssssssssewfrdseafrgvtF")
     all_words = words.words()
     res = ""
     for word in all_words:
@@ -15,3 +20,4 @@ def find_matching_words(pattern: str, include: str, exclude: str) -> str:
             and all(letter not in word for letter in exclude)
         ):
             res += word + "\n"
+    return res
